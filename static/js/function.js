c19d0682feea1b3a8e047c5b16c8a9e1049f47a3
NB777 <altmshfkgudtjr@naver.com>
2019-05-22 17:35:01 +0900

    2019-05-22 ver.2

--- static/js/function.js
+++ static/js/function.js
@@ -4,42 +4,61 @@ function menu_1()
     if(menu_cnt == 0)
     {
         $("#menu_1").addClass("menu_1_open");
-        $("#angle-down1").addClass("fa-angle-rotate");
+        $("#angle-down").addClass("fa-angle-rotate");
         menu_cnt += 1;
     }
     
     else
     {
         $("#menu_1").removeClass("menu_1_open");
-        $("#angle-down1").removeClass("fa-angle-rotate");
+        $("#angle-down").removeClass("fa-angle-rotate");
         menu_cnt = 0;
     }
 }
 
 
-var menu_login_cnt = 0;
-function menu_login()
+var select_location_cnt = 0;
+function select_location()
 {
-    if(menu_login_cnt == 0)
+    if(select_location_cnt == 0)
     {
-        $("#menu_login").addClass("menu_login_open");
-        $("#angle-down2").addClass("fa-angle-rotate");
-        menu_login_cnt += 1;
+        $("#select_location").addClass("select_location_open");
+        $("#angle-up").addClass("fa-angle-rotate");
+        select_location_cnt += 1;
     }
     
     else
     {
-        $("#menu_login").removeClass("menu_login_open");
-        $("#angle-down2").removeClass("fa-angle-rotate");
-        menu_login_cnt = 0;
+        $("#select_location").removeClass("select_location_open");
+        $("#angle-up").removeClass("fa-angle-rotate");
+        select_location_cnt = 0;
     }
 }
-
 
 $('#board1').click(function(e) 
 {
    $("#menu_1").removeClass("menu_1_open");
-   $("#angle-down1").removeClass("fa-angle-rotate");
-   $("#menu_login").removeClass("menu_login_open");
-   $("#angle-down2").removeClass("fa-angle-rotate");
+   $("#angle-down").removeClass("fa-angle-rotate");
+   $("#select_location").removeClass("select_location_open");
+   $("#select_location").removeClass("fa-angle-rotate");
+});
+
+
+
+
+
+
+$('#explain_gobutton1').click(function()
+{
+    $('#information2').addClass("none_display");
+    $('#information').removeClass("none_display");
+    $('#explain_gobutton2').removeClass("none_display");
+    $('#explain_gobutton1').addClass("none_display");
+});
+$('#explain_gobutton2').click(function()
+{
+    $('#information').addClass("none_display");
+    $('#information2').removeClass("none_display");
+    $('#explain_gobutton1').removeClass("none_display");
+    $('#explain_gobutton2').addClass("none_display");
 });
